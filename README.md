5e8d6188c9249069aae8dbc34e7cf0515448fdb1
Replit user <>
2022-11-23 04:12:46 +0000

    working prototype

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # codecademy_js_mixed_messages
+
+## The plan
+Using this space as a project plan as well:
+The plan is to create a function that takes 3 themes as an argument, and selects 3 random nouns according to those themes.
+
+The nouns are then placed into an Ad-Lib type structure, and the end phrase is returned.
+
+## Example:
++ theme 1 === "people"
++ theme 2 === "places"
++ theme 3 === "food"
+
+###Selected words (selected randomly using wordList[math.floor(math.random) * wordList.length])
++ word 1 === "Jerry Seinfeld"
++ word 2 === "Albuquerque"
++ word 3 === "eggs"
+
+###Phrase structure:
+`I just saw ${word1} in ${word2} eating ${word3}!`
+
+###Returned result:
+"I just saw Jerry Seinfeld in Albuquerque eating eggs!"
+
+Initial prototype will have this result, but long term, will add new theme word lists and phrase structures to make more varied results.
+
+### Components
+
+#### Data Objects
++ JSON formatted list of people
++ JSON formatted list of places
++ JSON formatted list of foods
+
+#### Functions
++ helper function: select random word from theme list
++ master function: select words using helper function, then return statement with selected words
+ 
+## Long Term Goals
++ connect this phrase generator to OpenAI's Dall-E API to make an AI image out of the generated phrase
---
+++ index.js
@@ -0,0 +1,45 @@
+// Creating data objects
+
+const people = ['Jerry Seinfeld', 'Rick James', 'Emilio Estevez', 'George Washington', 'Jair Bolsonaro', 'Zayne', 'Harry Style\'s stylist', 'Fidel Castro', 'Richard Nixon', 'Mac Dre', 'Tom Brady', 'Barry Bonds']
+
+const places =['Albequerque, New Mexico', 'Buenos Aires, Argentina', 'Mallorca, Spain', 'Guayabo National Monument, Costa Rica', 'Seattle, Washington, USA', 'Newark, Delaware, USA', 'Taipei, Taiwan']
+
+const foods = ['hard boiled eggs', 'plain, non-fat greek yogurt', 'bangers and mash', '300 mandarin oranges', 'fried plantains', 'an extra large order of McDonald\'s french fries', 'the delicious soft serve ice cream from Milanga & Co', 'a fully stuffed arepa']
+
+function reusable(strings) {
+  return function(... vals) {
+    return strings.map(function(s, i) {
+      return `${s}${vals[i] || ""}`;
+    }).join("");
+  };
+}
+
+const messageFormats = ["I just saw ${person} in ${place} eating ${food}!", "Did you know that at this very moment in ${place}, ${person} is eating ${food}? It's true. Trust me, I'm a doctor.", "In my wildest dreams I would never expect to see ${food} being eaten by ${person} in ${place}, but I can't deny reality."]
+
+
+// Helper functions
+
+// found a module that uses RegEx to fill in vars that are declared as part of the normal string
+const fillTemplate = require('es6-dynamic-template');
+
+const selectRandom = list => list[Math.floor(Math.random() * list.length)];
+
+const combineWords = () => {
+  let person = selectRandom(people);
+  let place = selectRandom(places);
+  let food = selectRandom(foods);
+  let format = selectRandom(messageFormats);
+
+  let message = fillTemplate(format, 
+    {
+    person: person,
+    place: place,
+    food: food
+    });
+
+
+  return message;
+}
+
+console.log(combineWords());
+
